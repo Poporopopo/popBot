602288ed0f7a7f619f34e0b50b9092782bdb09f4
Poporopopo <poporopyon@gmail.com>
2020-07-29 13:57:03 -0400

    testing bot commands bot.process_commands will ignore bots so we'll have to make our own

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
 import discord, pathlib
+# bot commands lib
+from discord.ext import commands
 
 file_parent_location = str(pathlib.Path(__file__).parent)
 client = discord.Client()
+bot = commands.Bot(command_prefix='popbot ')
 
 #  callback: happens when an event happens
-@client.event
+@bot.event
 async def on_ready():
     # prints in terminal that bot is online
     print('We have logged in as {0.user}'.format(client))
 
 # reads messages
-@client.event
+@bot.event
 async def on_message(message):
-    # exits when message is from this bot
-    # otherwise (including other bots) will respond
-    if message.author == client.user:
-        return
+    print("The message's content was", message.content)
 
-    # if the message is from a user and as the key, will respond with Hello
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    # # testing: works for tupper bots
+    # print(message.author.name)
+    # print (message.author.name == "Poporopopo")
+    # if message.author.name == "Poporopopo":
+    #     return
 
+    message_context = await bot.get_context(message)
+    message_author = message_context.author
+    message_author_name = message_author.name
+    # message_author_nickname = message_author.nick
+    message_author_isBot = message_author.bot
+    print (message_context)
+    print (message_author)
+    print (message_author_name)
+    # print (message_author_nickname)
+    print (message_author_isBot)
+    #ignores bots
+    await bot.process_commands(message)
+
+# commands section
+@bot.command()
+async def echo(ctx, *, content:str):
+    await ctx.send(content)
+
+# run section; must be at end
 bot_token = ""
 with open(file_parent_location + "/data/bot_token.txt", "r") as tokenfile:
     bot_token = tokenfile.readline()
-client.run(bot_token)
+bot.run(bot_token)
