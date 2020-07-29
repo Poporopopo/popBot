00126d5aec3ae8b09697c9cb21bdd3e42e91a1a4
Poporopopo <poporopyon@gmail.com>
2020-07-29 16:44:41 -0400

    Testing channel reading commands

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import discord, pathlib
 # bot commands lib
 from discord.ext import commands
 
+# bot token finder
 file_parent_location = str(pathlib.Path(__file__).parent)
+bot_token = ""
+with open(file_parent_location + "/data/bot_token.txt", "r") as token_file:
+    bot_token = token_file.readline()
+
 client = discord.Client()
 bot = commands.Bot(command_prefix='popbot ')
 
@@ -10,7 +15,7 @@ bot = commands.Bot(command_prefix='popbot ')
 @bot.event
 async def on_ready():
     # prints in terminal that bot is online
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
 # reads messages
 @bot.event
@@ -27,22 +32,50 @@ async def on_message(message):
     message_author = message_context.author
     message_author_name = message_author.name
     # message_author_nickname = message_author.nick
+
+    # context is immutable
+    # try:
+    #      message_author.bot = False
+    # except Exception as error:
+    #     print (error)
+
     message_author_isBot = message_author.bot
     print (message_context)
     print (message_author)
     print (message_author_name)
     # print (message_author_nickname)
     print (message_author_isBot)
+    print()
     #ignores bots
     await bot.process_commands(message)
 
 # commands section
+# example command
 @bot.command()
 async def echo(ctx, *, content:str):
     await ctx.send(content)
 
+@bot.command()
+async def test(ctx):
+    channel = ctx.channel
+    async for message in channel.history():
+        print (message.author)
+        print ()
+
+# tracks sessions based on channels
+sessions = []
+
+# marks the channel as an open session
+@bot.command()
+async def open(ctx):
+    # shows the channel marked
+    channel_name = ctx.channel
+    print (channel_name)
+    sessions.append(channel_name)
+    print (sessions)
+    await ctx.send(f'Session has been opened in {channel_name}')
+
+
+
 # run section; must be at end
-bot_token = ""
-with open(file_parent_location + "/data/bot_token.txt", "r") as tokenfile:
-    bot_token = tokenfile.readline()
 bot.run(bot_token)
