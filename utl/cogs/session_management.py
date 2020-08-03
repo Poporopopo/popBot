07ac03869ce05e8b194e182e3e13c476d1a4cb62
Poporopopo <poporopyon@gmail.com>
2020-08-03 15:27:32 -0400

    added functions to join, check if joined, check if session

--- utl/cogs/session_management.py
+++ utl/cogs/session_management.py
@@ -11,11 +11,11 @@ class session_cog(commands.Cog):
     async def open(self, ctx):
         # shows the channel marked
         channel = ctx.channel
-        # quits if session is moving
-        if (self.in_session(channel.id, self.sessions)):
+        # quits if session is recording
+        if self.is_in_session(channel.id):
             return
-        all_sessions = self.sessions.keys()
-        if (channel.id not in all_sessions):
+
+        if self.is_session_not_open(channel.id):
             self.sessions[channel.id] = [[],[]]
             await ctx.send(f'Session has been opened in {channel}.')
         else:
@@ -27,8 +27,8 @@ class session_cog(commands.Cog):
     @commands.command()
     async def close(self, ctx):
         channel = ctx.channel
-        # quits if session is moving
-        if (self.in_session(channel.id, self.sessions)):
+        # quits if session is recording
+        if self.is_in_session(channel.id):
             return
         try:
             # pauses session and checks for title
@@ -42,14 +42,50 @@ class session_cog(commands.Cog):
         print ("Sessions:",
             self.sessions)
 
+    # adds character to current cast
+    @commands.command()
+    async def join(self, ctx, *args):
+        if self.is_session_not_open(ctx.channel.id):
+            await ctx.send(
+                f'Session is not open in {ctx.channel}.'
+            )
+            return
+        if len(args) > 0 and args[0] == 'bot':
+            print (args[0])
+            return
+        # add message sender to cast of session
+        print("Is in cast?: ",
+            self.is_in_cast(ctx.author.display_name, ctx.channel.id)
+        )
+        if self.is_in_cast(ctx.author.display_name, ctx.channel.id):
+            await ctx.send(
+                f'{ctx.author.display_name}'
+                f' is already in the session in {ctx.channel}.'
+            )
+        else:
+            self.sessions[ctx.channel.id][0].append(ctx.author.display_name)
+            await ctx.send(
+                f'{ctx.author.display_name}'
+                f' has been added session in {ctx.channel}.'
+            )
+
+    # check if person is in the cast
+    # author is string, and channel id is int
+    def is_in_cast(self, author, channel_id):
+        return author in self.sessions[channel_id][0]
+
+    # check if session is open
+    def is_session_not_open(self, channel_id):
+        return (channel_id not in self.sessions.keys())
+
     # determines if a session is being recorded
     # returns boolean
-    def in_session(self, channel_id, sessions_dict):
+    def is_in_session(self, channel_id):
         # check if sessions_dict is empty
-        if len(sessions_dict) < 1:
+        if len(self.sessions) < 1:
             return False
         # check if there was a pause command issued
-        session_sections = sessions_dict[channel_id][1]
+        session_sections = self.sessions[channel_id][1]
         if len(session_sections) < 1:
             return False
         return len(session_sections[-1][1]) == 1
