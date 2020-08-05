286f3dd2191264f56a87bab059c93d4147b5edae
Poporopopo <poporopyon@gmail.com>
2020-08-04 23:47:43 -0400

    added check for if session is paused

--- utl/classes/session.py
+++ utl/classes/session.py
@@ -14,11 +14,21 @@ class session:
 
     # creates a new section
     def start_section(start_date):
-        section = section(cast, start_date)
+        # if the previous section is still open, will not make a new one
+        if (not is_paused()):
+            return
+        section = section(self.cast, start_date)
         self.cast.append(section)
         return
 
-def section:
+    # checks if the latest section is closed
+    def is_paused():
+        if (len(self.cast) < 1):
+            return True
+        last_section = self.cast[-1]
+        return (not last_section.is_open())
+
+class section:
     def __init__(self, cast, start_date):
         self.cast = cast.copy()
         self.start_date = start_date
