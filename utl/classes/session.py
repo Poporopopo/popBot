66fbcaefd399e821785425e65d956bc6da040f56
Poporopopo <poporopyon@gmail.com>
2020-08-05 11:07:31 -0400

    Implemented error raising in session methods

--- utl/classes/session.py
+++ utl/classes/session.py
@@ -1,4 +1,4 @@
-from exceptions import *
+from exceptions.session_exceptions import *
 
 class session:
     def __init__(self, id ,cast=[], sections=[]):
@@ -17,14 +17,14 @@ class session:
 
     # takes name <string> add pushes to cast array
     def add_member(self, name):
-        if name in cast:
+        if name in self.get_cast():
             raise (Cast_Error("Member already added"))
         else:
             self.cast.append(name)
 
     # takes name <string> and removes from cast array
     def remove_member(self, name):
-        if name in cast:
+        if name in self.get_cast():
             self.cast.remove(name)
         else:
             raise (Cast_Error("Member not in Cast"))
@@ -33,7 +33,7 @@ class session:
     def start_section(self, start_date):
         # if the previous section is still open, will not make a new one
         if (not self.is_paused()):
-            return
+            raise (Pause_Error("A section is already open"))
         new_section = section(self.cast, start_date)
         self.sections.append(new_section)
         return
@@ -44,7 +44,7 @@ class session:
         # previous section is still opened
         # no section exists
         if self.is_paused():
-            return
+            raise (Pause_Error("No sections to close"))
         last_section = self.sections[-1]
         last_section.close(stop_date)
 
--- utl/exceptions/session_exceptions.py
+++ utl/exceptions/session_exceptions.py
@@ -4,3 +4,10 @@ class Cast_Error(Exception):
 
     def __str__(self):
         return (repr(self.value))
+
+class Pause_Error(Exception):
+    def __init__ (self, value):
+        self.value = value
+
+    def __str__(self):
+        return (repr(self.value))
