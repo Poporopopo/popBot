909de935f674d99e6b367b11ede10cd537477299
Poporopopo <poporopyon@gmail.com>
2020-08-05 00:39:22 -0400

    Tested section object and added self argument to all methods

--- utl/classes/session.py
+++ utl/classes/session.py
@@ -45,23 +45,23 @@ class section:
         self.start_date = start_date
         self.end_date = None
 
-    def get_cast():
+    def get_cast(self):
         return self.cast
 
-    def get_start():
+    def get_start(self):
         return self.start_date
 
-    def get_end():
+    def get_end(self):
         return self.end_date
 
-    def set_end(end_date):
+    def set_end(self, end_date):
         self.end_date = end_date
 
     # checks if the section is closed
-    def is_open():
+    def is_open(self):
         return self.get_end() == None
 
     # closes the section with a stop date
-    def close(stop_date):
+    def close(self, stop_date):
         if self.is_open():
             self.set_end(stop_date)
