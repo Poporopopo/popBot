da458ba9de0e5ed3493c3f371e35794c228f5283
Poporopopo <poporopyon@gmail.com>
2020-08-07 21:52:35 -0400

    added start_section testing

--- tester_file.py
+++ tester_file.py
@@ -1,5 +1,6 @@
 from utl.classes import session_manager, session, section
 
+# Section testing
 print ("Testing section")
 print ("Initializing section")
 cast = ["guy 1", "guy 2"]
@@ -14,6 +15,7 @@ print ("close(date):", sec.close(2021))
 print ("Section at the end:", sec)
 print ()
 
+# session testing
 print ("Testing session")
 print ("Initializing session")
 sess = session.session(22222)
@@ -25,12 +27,12 @@ print ("get_cast():", sess.get_cast())
 print ("get_sections:", sess.get_sections())
 print ()
 
+# session testing: cast methods
 print ("Testing section cast methods")
 print ("Adding to cast new member")
 try:
     sess.add_member("Guy 1")
     print ("Guy 1 added")
-    print (sess.get_cast())
 except session.Cast_Error as error:
     print ("Guy 1 not added.", error)
 
@@ -38,7 +40,6 @@ print ("Adding to cast existing member")
 try:
     sess.add_member("Guy 1")
     print ("Guy 1 added")
-    print (sess.get_cast())
 except session.Cast_Error as error:
     print ("Guy 1 not added.", error)
 
@@ -47,6 +48,7 @@ guy_number = 2
 while guy_number <= 100:
     sess.add_member(f'Guy {guy_number}')
     guy_number += 1
+del guy_number
 print ("Cast after bulk adds:", sess.get_cast())
 
 print ("Removing from cast existing member")
@@ -62,7 +64,45 @@ try:
     print ("Guy 2 removed")
 except session.Cast_Error as error:
     print ("Guy 2 not removed.", error)
+print()
 
+# session testing: section creation
+sess.cast = []
+print ("Start a section with empty cast")
+try:
+    self.start_section(2020)
+    print ("Section added")
+except Pause_Error as error:
+    print error
+except Cast_Error as error:
+    print error
+print()
+
+guy_number = 1
+while guy_number <= 100:
+    sess.add_member(f'Guy {guy_number}')
+    guy_number += 1
+del guy_number
+
+print ("Starting a section with a cast")
+try:
+    self.start_section(2020)
+    print ("Section added")
+except Pause_Error as error:
+    print error
+except Cast_Error as error:
+    print error
+print ()
+
+print ("Starting a section with one already started")
+try:
+    self.start_section(2020)
+    print ("Section added")
+except Pause_Error as error:
+    print error
+except Cast_Error as error:
+    print error
+print()
 # print ("Testing session manager")
 #
 # print("initializing session manager object")
